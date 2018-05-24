8981ae6b5252b13e03bf31b456c78127253d0f69
Gabor Koncsar <gabor.koncsar@gmail.com>
2018-05-24 20:28:24 +0200

    first commit

---
+++ src/dateproba/dateproba.java
@@ -0,0 +1,14 @@
+package dateproba;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+public class dateproba {
+
+	public static void main(String[] args) {
+
+	}
+}
